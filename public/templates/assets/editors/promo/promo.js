85eb29f6ad8369e7fa569d0d311ce8da3b1d2a6f
jflowers45 <jflowers45@gmail.com>
2013-09-13 18:55:56 +0000

    POP-28 #comment Created promo plugin

---
+++ public/templates/assets/editors/promo/promo.js
@@ -0,0 +1,22 @@
+/*global EditorHelper*/
+
+EditorHelper.addPlugin( "promo", function( trackEvent ) {
+  var _container,
+      target;
+
+  _container = trackEvent.popcornTrackEvent._container;
+  target = trackEvent.popcornTrackEvent._target;
+
+  if ( window.jQuery ) {
+    /*
+	  if ( trackEvent.popcornOptions.position === "custom" ) {
+      EditorHelper.draggable( trackEvent, _container, target );
+      EditorHelper.resizable( trackEvent, _container, target, {
+        minWidth: 10,
+        handlePositions: "e,w"
+      });
+    }
+    */
+    //EditorHelper.contentEditable( trackEvent, _container.querySelectorAll( "span" ) );
+  }
+});
---
+++ public/templates/assets/plugins/promo/popcorn.promo.js
@@ -0,0 +1,243 @@
+// PLUGIN: promo
+(function ( Popcorn ) {
+	function newlineToBreak( string ) {
+		// Deal with both \r\n and \n
+		return string.replace( /\r?\n/gm, "<br>" );
+	}  
+	
+	Popcorn.plugin( "promo", {
+		manifest: {
+			about:{
+				name: "Popcorn promo Plugin",
+				version: "0.1",
+				author: "bbginnovate",
+				website: "http://www.innovation-series.com/"
+			},
+			options:{
+				start: {
+					elem: "input",
+					type: "number",
+					label: "Start"
+				},
+				end: {
+					elem: "input",
+					type: "number",
+					label: "End"
+				},
+				label: {
+					elem: "input",
+					type:"textarea",
+//					editor: "ckeditor",
+					label: "Label",
+					"default": "PROMO-label"
+				},
+				headline1: {
+					elem: "input",
+					type: "textarea",
+					label: "Headline1",
+					"default": "This is the default headline1  and it can span a few lines"
+				},
+				imageURL1: {
+					elem: "input",
+					type: "textarea",
+					label: "Image URL1",
+					"default": "../assets/images/promo-red.jpg"
+				},
+				linkURL1: {
+					elem: "input",
+					type: "textarea",
+					label: "Link URL1",
+					"default": "http://google.com?link1"
+				},
+				headline2: {
+					elem: "input",
+					type: "textarea",
+					label: "Headline2",
+					"default": "This is the default headline2 and it can span a few lines"
+				},
+				imageURL2: {
+					elem: "input",
+					type: "textarea",
+					label: "Image URL2",
+					"default": "../assets/images/promo-blue.jpg"
+				},
+				linkURL2: {
+					elem: "input",
+					type: "textarea",
+					label: "Link URL2",
+					"default": "http://google.com?link2"
+				},
+				headline3: {
+					elem: "input",
+					type: "textarea",
+					label: "Headline3",
+					"default": "This is the default headline3  and it can span a few lines"
+				},
+				imageURL3: {
+					elem: "input",
+					type: "textarea",
+					label: "Image URL3",
+					"default": "../assets/images/promo-green.png"
+				},
+				linkURL3: {
+					elem: "input",
+					type: "textarea",
+					label: "Link URL3",
+					"default": "http://google.com?link3"
+				},
+				rssFeed: {
+					elem:"input",
+					type:"textarea",
+					label:"RSS Feed",
+					group:"advanced"
+				},
+				zindex: {
+		          hidden: true
+		        }
+
+			} //end options
+		}, //end manifest
+
+		_setup: function( options ) {
+			var DEFAULT_FONT_COLOR = "#000000",
+			DEFAULT_SHADOW_COLOR = "#444444",
+			DEFAULT_BACKGROUND_COLOR = "#888888";
+			
+			
+			/*
+			function getPromoStr(promos) {
+				//promos is an array of <= 3
+				var str="";
+				for (i=1; i<promos.length;i++) {
+					var p = promos[i];
+					str += "<div c";
+					
+				}
+			}
+			*/
+			  
+			var target = Popcorn.dom.find( options.target );
+			if ( !target ) {
+				target = this.media.parentNode;
+			}
+			options._target = target;
+			var label=newlineToBreak(options.label);
+			
+			var rssURL=options.rssURL;
+			
+			if (rssURL != null && rssUrl != "") {
+				/*
+				Popcorn.getJSONP(
+					apiURL,
+					function( data ) {
+					  storifyAsJson=data;
+					  numStories=data.content.elements.length;
+					  storifyResultString=JSON.stringify(data); 
+					}
+				 );
+				 */
+			}
+			
+			var context = this;
+			
+			//CREATE OUR CONTAINER AND STYLE IT
+			var promoBackground = document.createElement( "div" );
+			promoBackground.setAttribute("id", "promoBackground");
+			promoBackground.classList.add( "off" );
+			options.promoBackground=promoBackground
+			target.appendChild( promoBackground ); 
+			options._container=promoBackground; 	//we have to add that line so that editor gets access
+			options.toString = function() {
+			   //this is the value that shows up in the track editor text
+				var str="Promo-label";
+				if (options.label != null) {
+					str=options.label;
+				}
+				return str;
+			};
+		
+		},	//end 'setup' fx
+      
+		start: function(event, options){
+			// options.container.innerHTML="HELLO WORLD";
+			var redrawBug;
+			if ( options.promoBackground ) {
+				options.promoBackground.classList.add( "on" );
+				options.promoBackground.classList.remove( "off" );
+				
+				var linkURL="www.voanews.com";
+				if (options.linkURL != null && options.linkURL !="") {
+					linkURL=options.linkURL;
+				}
+				var linkTitle=linkURL;
+								
+				var htmlStr="";
+				var titleStr="theTitle"
+				var descStr="theDesc";
+				
+				/*
+				var linkStr="<a id='promoLink' href='"+linkURL+"'>"+linkTitle+"</a>";
+				htmlStr+="<div id='promoBackground'>";
+				htmlStr+="<div id='promoTitle'>" + titleStr  + "</div>";
+				htmlStr+="<div id='promoDescription'>" + descStr + "</div><hr id='promoHR' />";
+				htmlStr+=linkStr;
+				htmlStr+="</div>"; //end lowerThirdBG
+				*/
+				
+				var promoLabel=newlineToBreak(options.label);
+				
+				var headline1=newlineToBreak(options.headline1);
+				var imageURL1=newlineToBreak(options.imageURL1);
+				var linkURL1=newlineToBreak(options.linkURL1);
+				
+				var headline2=newlineToBreak(options.headline2);
+				var imageURL2=newlineToBreak(options.imageURL2);
+				var linkURL2=newlineToBreak(options.linkURL2);
+				
+				var headline3=newlineToBreak(options.headline3);
+				var imageURL3=newlineToBreak(options.imageURL3);
+				var linkURL3=newlineToBreak(options.linkURL3);
+				
+				htmlStr+="<table  id='promoTable' width='100%' cellpadding='10'>";
+				htmlStr+="<tr>";
+				htmlStr+="<td></td>";
+				htmlStr+="<td colspan='3' class='promoLabel'>" + promoLabel + "</td>";
+				htmlStr+="<td></td>";
+				htmlStr+="</tr>";
+				htmlStr+="<tr>";
+				htmlStr+="<td></td>";
+				htmlStr+="<td><a target='_blank' class='promoLink' href='"+linkURL1 + "'><img src='"+imageURL1+"' width='100%'></a></td>";
+				htmlStr+="<td><a target='_blank' class='promoLink' href='"+linkURL2 + "'><img src='"+imageURL2+"' width='100%' ></a></td>";
+				htmlStr+="<td><a target='_blank' class='promoLink' href='"+linkURL2 + "'><img src='"+imageURL3+"' width='100%'></a></td>";
+				htmlStr+="<td></td>";
+				htmlStr+="</tr>";
+				htmlStr+="<tr>";
+				htmlStr+="<td width='6%'></td>";
+				htmlStr+="<td width='29%'><a target='_blank' class='promoLink' href='"+linkURL1 + "'>"+headline1+"</a></td>";
+				htmlStr+="<td width='30%'><a target='_blank' class='promoLink' href='"+linkURL2 + "'>"+headline2+"</a></td>";
+				htmlStr+="<td width='29%'><a target='_blank' class='promoLink' href='"+linkURL3 + "'>"+headline3+"</a></td>";
+				htmlStr+="<td width='6%'></td>";
+				htmlStr+="</tr>";
+				htmlStr+="</table>";
+				/*
+				htmlStr+="<table>";
+				htmlStr+="<tr><td><img src='../assets/images/logo_alhurra.png'></td><td>headline<BR>link</td></tr>";
+				htmlStr+="<tr><td>&nbsp;</td></tr>";
+				htmlStr+="<tr><td><img src='../assets/images/logo_alhurra.png'></td><td>headline<BR>link</td></tr>";
+				htmlStr+="<tr><td>&nbsp;</td></tr>";
+				htmlStr+="<tr><td><img src='../assets/images/logo_alhurra.png'></td><td>headline<BR>link</td></tr>";
+				htmlStr+="</table>";
+				*/
+				
+				options.promoBackground.innerHTML=htmlStr;
+				
+			}
+		},	//end 'start' fx
+		end: function( event, options ) {
+			if ( options.promoBackground ) {
+				options.promoBackground.classList.remove( "on" );
+				options.promoBackground.classList.add( "off" );
+			}
+		}//end 'end' fx
+  });	//end 'promo' line and 'popcorn.plugin'
+}( window.Popcorn ));	//(function ( Popcorn ) {

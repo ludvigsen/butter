5949fbcde89acb0caec7f1191729fa1ad8d80973
root <jflowers45@gmail.com>
2013-11-14 21:03:57 +0000

    Ckeditor style issues POP-200

--- public/external/ckeditor/config.js
+++ public/external/ckeditor/config.js
@@ -24,11 +24,19 @@ CKEDITOR.editorConfig = function( config ) {
 	
  
 	//config.font_names = 'Merriweather;Gentium Book Basic;Lato;Vollkorn;Gravitas One;PT Sans;Open Sans;Bangers;Fredoka One;Covered By Your Grace;Coda';
-	config.font_names = 'Merriweather;Open Sans;Bangers;Fredoka One;Coda';
-
+	//config.font_names = 'Merriweather;Open Sans;Bangers;Fredoka One;Coda';
+	
+	config.font_names = 'Verdana;Trebuchet MS'; 
+	myFonts = ['Merriweather', 'Open Sans', 'Bangers', 'Fredoka One', 'Coda'];
+	for(var i = 0; i<myFonts.length; i++){
+		config.font_names = config.font_names+';'+myFonts[i];
+		myFonts[i] = 'http://fonts.googleapis.com/css?family='+myFonts[i].replace(' ','+');
+	}
+	config.contentsCss = ['/css/kettlecorn-editor.css'].concat(myFonts);
+	
 	config.extraPlugins = 'stylesheetparser,translate';
 	config.stylesSet = []; // don't load the default style rules - only load from CSS
-	config.contentsCss = '/css/kettlecorn-editor.css'; // load the styling for standard elements as well as pre-defined class options
+	//config.contentsCss = '/css/kettlecorn-editor.css'; // load the styling for standard elements as well as pre-defined class options
 	
 	config.fontSize_sizes = 'xx-small/.2em;x-small/.5em;small/.8em;medium/1em;large/1.2em;x-large/1.5em;xx-large/2em';
 };
